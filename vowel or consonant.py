0e6b353924d8be23d1bc15104ca8c54b24d84d33
Gurushankar55 <gurushankarcvp@gmail.com>
2018-07-23 12:17:06 +0530

    Create vowel or consonant.py

---
+++ vowel or consonant.py
@@ -0,0 +1,7 @@
+1=input("input a letter of the object: ")
+if 1 in('a','e','i','o','u'):
+print("%s is a vowel." % 1)
+elif 1 == 'y';
+print("Sometimes letter y stand for vowel, sometimes it stand for consonant.")
+else:
+print("%s is the consonant." % l) 
